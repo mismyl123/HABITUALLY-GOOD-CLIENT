d4a27b1003c993000ca0649bceafad0c0a65939b
mismyl123 <mlalonde309@gmail.com>
2020-11-15 20:47:26 -0500

    starting point for login/registration forms

---
+++ src/services/token-services.js
@@ -0,0 +1,6 @@
+makeBasicAuthToken(userName, password) {
+    return window.btoa(`${userName}:${password}`)
+  },
+  saveAuthToken(token) {
+    window.localStorage.setItem(config.TOKEN_KEY, token)
+  },
