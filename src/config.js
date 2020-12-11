af0b03feb2c0cd684789bfcc59604ba7507b81ce
mismyl123 <mlalonde309@gmail.com>
2020-12-10 22:30:23 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://serene-sierra-18729.herokuapp.com',
+    API_ENDPOINT: '',
    //API_ENDPOINT: 'https://localhost:8080',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'habitually-client-token-key'
