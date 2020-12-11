42d65b29cbf354e8f7d24d1fb6eb6137ed4d2260
mismyl123 <mlalonde309@gmail.com>
2020-12-10 23:56:39 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://damp-castle-21568.herokuapp.com/',
+    API_ENDPOINT: 'https://damp-castle-21568.herokuapp.com',
    //API_ENDPOINT: 'https://localhost:8080',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'habitually-client-token-key'
